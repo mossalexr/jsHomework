088a44f27cfb3b9a51d9a43f01c047f32789f306
AlexMoss <moss.alexr@gmail.com>
2022-07-28 20:15:41 -0400

    push

---
+++ app.js
@@ -0,0 +1,19 @@
+function addSum(param1, param2){
+    console.log(param1 + param2);
+}
+addSum(10,20);
+
+function diff(param1, param2){
+    console.log(param1 - param2);
+}
+diff(10, 20);
+
+function mult(param1, param2){
+    console.log(param1 * param2);
+}
+mult(10, 20);
+
+function div(param1, param2){
+    console.log(param1 / param2);
+}
+div(10, 20);
